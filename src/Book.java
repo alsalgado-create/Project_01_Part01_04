4b5448bde33bb0ace9853f39379ca0ccd7130aeb
Alex <asalgado@csumb.edu>
2025-09-20 11:44:09 -0700

    Added the private instance fields for Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,22 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Book {
+
+    // Constant fields these are used for CSV indexing.
     public static final int ISBN_ = 0;
     public static final int TITLE_ = 1;
     public static final int SUBJECT_ = 2;
     public static final int PAGE_COUNT_ = 3;
     public static final int AUTHOR_ = 4;
     public static final int DUE_DATE_ = 5;
+
+    // Instance Fields.
+    private String author;
+    private LocalDate dueDate;
+    private String isbn;
+    private int pageCount;
+    private String subject;
+    private String title;
+
 }
