1b73dbecf6a9811b5f55ca0072620e148958297f
Alex <asalgado@csumb.edu>
2025-09-20 11:33:44 -0700

    Created Book.java and added the hashCode for it.

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    public static final int ISBN_ = 0;
+    public static final int TITLE_ = 1;
+    public static final int SUBJECT_ = 2;
+    public static final int PAGE_COUNT_ = 3;
+    public static final int AUTHOR_ = 4;
+    public static final int DUE_DATE_ = 5;
+}
