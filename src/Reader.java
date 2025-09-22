f2b93a31a9fcfa55b794d0e30d2675ee67b5938f
Alex <asalgado@csumb.edu>
2025-09-21 19:27:32 -0700

    Created a new branch named Reader as well as added the already provided ReaderTest.java file as well as began writing code for Reader.java

---
+++ src/Reader.java
@@ -0,0 +1,16 @@
+/**
+ * @author Alexis Salgado
+ * @since 2025.09.21
+ * Abstract: This is the Reader.java File
+ *
+ *
+ *
+ */
+public class Reader {
+    public static final int CARD_NUMBER_ = 0;
+    public static final int NAME_ = 1;
+    public static final int PHONE_ = 2;
+    public static final int BOOK_COUNT_ = 3;
+    public static final int BOOK_START_ = 4;
+
+}
