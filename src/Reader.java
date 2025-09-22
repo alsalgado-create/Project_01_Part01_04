351adfa3d1c01e5bc5a8ff96056d036f6188a881
Alex <asalgado@csumb.edu>
2025-09-21 19:33:50 -0700

    Created the Instance fields for Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * @author Alexis Salgado
  * @since 2025.09.21
@@ -7,10 +9,17 @@
  *
  */
 public class Reader {
+    // Constant fields used for CSV indices
     public static final int CARD_NUMBER_ = 0;
     public static final int NAME_ = 1;
     public static final int PHONE_ = 2;
     public static final int BOOK_COUNT_ = 3;
     public static final int BOOK_START_ = 4;
+
+    // Instance fields
+    private int cardNumber;
+    private String name;
+    private String Phone;
+    private List<Book> books;
 
 }
