23ac947487b2312d1cc7e708d696421e61c5fc7b
Alex <asalgado@csumb.edu>
2025-09-28 17:28:48 -0700

    Added the import hashmap, map and objects to Shelf.java. Created Constant fields as well as the instance fields.

--- src/Shelf.java
+++ src/Shelf.java
@@ -1,3 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 public class Shelf {
+    //Constant Fields
+    public static final int SHELF_NUMBER_ = 0;
+    public static final int SUBJECT_ = 1;
+
+    // Creates Instance fields.
+    private HashMap<Book, Integer> books = new HashMap<>();
+    private int shelfNumber;
+    private String subject;
 
 }
