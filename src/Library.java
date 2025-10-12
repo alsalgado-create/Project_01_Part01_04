abe4b9db3ebdd16a4b2abaa54869e0e34431b7e6
Alex <asalgado@csumb.edu>
2025-10-12 16:52:01 -0700

    Created the Constant and Instance fields for the Library class.

--- src/Library.java
+++ src/Library.java
@@ -11,5 +11,18 @@ import Utilities.Code;
 import java.util.*;
 import java.time.LocalDate;
 public class Library {
-
+    /**
+     * Constant fields
+     */
+    public static final int NAME_ = 0;
+    public static final int ADDRESS_ = 1;
+    public static final int PHONE_ = 2;
+    /**
+     * Instance fields
+     */
+    private String name;
+    private String address;
+    private String phone;
+    private HashMap<Integer, Reader> readers = new HashMap<>();
+    private HashMap<Integer, Shelf> shelves = new HashMap<>();
 }
